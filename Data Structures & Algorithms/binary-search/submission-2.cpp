0f4ff913a711ddd7944b847495ce064b01383c44
Toprak2 <muhammedalitoprak1@gmail.com>
2026-08-31 17:59:12 +0300

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int start = 0;
+        int end = nums.size()-1;
+
+        while(start <= end ){
+
+            int middle = start + (end - start)/2;
+
+            if(nums[middle] == target){
+                return middle;
+            }
+            else if(nums[middle] > target){
+                end = middle -1;
+            }
+            else{
+                start = middle +1;
+            }
+
+        }
+        return -1;
+
+    }
+};
